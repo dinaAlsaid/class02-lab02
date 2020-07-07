bf1b449c7c2f0fdec30a4fe804c29ce9ad40196e
dinaAlsaid <dinamalsaid@gmail.com>
2020-07-07 15:55:44 +0300

    update readme file

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # class02-lab02
-v 01.1
-new features:
 
-* 
-* 
-* 
-* 
+A guessing game about the developer of this game. you have to guess personal and general information about the developer.
+
+***Features:***
+
+**v 1.0**
+
+* 5 questions
+* Takes yes/no answers
+
+**v 1.1**
+
+* two extra questions added
+* takes numbers as answers
+* the user can have a number of guesses for some questions
+* Track the score of the user
+* A list of Top Ten
+* changes on the styles
--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ alert("this is a guessing game, you have to guess things about ME, dina");
 //// take the user's name
 var name = prompt("what is your name?");
 console.log("hello " + name + " !! let's play.");
-// alert("hello " + name + "!! let's play.");
+alert("hello " + name + "!! let's play.");
 
 
 ////-----question 1-----////
@@ -15,10 +15,10 @@ var age = prompt("am I older than 25 years old?");
 if (age.toLowerCase() == "no" || age.toLowerCase() == "n") {
     correctAnswers++;
     console.log("you're right, I am 24");
-    // alert("you're right, I am 24");
+    alert("you're right, I am 24");
 } else {
     console.log("Nope, I am 24");
-    // alert("Nope, I am 24");
+    alert("Nope, I am 24");
 
 }
 
@@ -27,10 +27,10 @@ var cat = prompt("do I like cats?");
 if (cat.toLowerCase() === "yes" || cat.toLowerCase() === "y") {
     correctAnswers++;
     console.log("correct");
-    // alert("correct");
+    alert("correct");
 } else {
     console.log("wrong, i do like cats");
-    // alert("wrong, I LOVE cats!!");
+    alert("wrong, I LOVE cats!!");
 
 }
 
@@ -39,10 +39,10 @@ var coffee = prompt("do I drink coffee");
 if (coffee.toLowerCase() === "no" || coffee.toLowerCase() === "n") {
     correctAnswers++;
     console.log("correct");
-    // alert("correct");
+    alert("correct");
 } else {
     console.log("wrong, Coffee is a no-no to me");
-    // alert("wrong, Coffee is a no-no to me");
+    alert("wrong, Coffee is a no-no to me");
 
 }
 
@@ -51,10 +51,10 @@ var climb = prompt("have I ever climbed a tree?");
 if (climb.toLowerCase() === "no" || climb.toLowerCase() === "n") {
     correctAnswers++;
     console.log("correct");
-    // alert("correct");
+    alert("correct");
 } else {
     console.log("wrong, i have weak arms");
-    // alert("wrong, I have weak arms");
+    alert("wrong, I have weak arms");
 
 }
 
@@ -63,10 +63,10 @@ var height = prompt("Am I shorter than 163cm?");
 if (height.toLowerCase() === "yes" || height.toLowerCase() === "y") {
     correctAnswers++;
     console.log("correct, i am 160 cm tall");
-    // alert("correct, i am 160 cm tall");
+    alert("correct, i am 160 cm tall");
 } else {
     console.log("wrong, i am 160 cm tall");
-    // alert("wrong, i am 160 cm tall");
+    alert("wrong, i am 160 cm tall");
 
 }
 
@@ -76,24 +76,24 @@ var howManyCats = prompt("How mant cats I had in my life? you have 4 chances to 
 for (var c = 1; c <= 3; c++) {
     if (howManyCats > 6) {
         console.log("too high");
-        // alert("too high");
+        alert("too high");
         howManyCats = prompt("too high,try again");
     } else if (howManyCats < 6) {
         console.log("too low");
-        // alert("too low");
+        alert("too low");
 
         howManyCats = prompt("too low, try again");
 
     } else if (howManyCats == 6) {
         correctAnswers++;
         console.log("you're correct");
-        // alert("you're correct");
+        alert("you're correct");
         break;
     }
 }
 if (howManyCats != 6) {
     console.log("you're out of chances, the correct answer is 6");
-    //alert("you're out of chances, the correct answer is 6");
+    alert("you're out of chances, the correct answer is 6");
 }
 
 
@@ -110,18 +110,23 @@ for (var i = 5; i >= 0; i--) { //for number of guesses
         if (guessThings == things[index] || guessThings == things[index] + "s") {
             flag = true;
         }
-
     }
     if (flag) {
         correctAnswers++;
         console.log("correct");
+        alert("correct");
         break;
-    } else { console.log("try again, you have " + i + " guesses left."); }
+    } else { 
+        console.log("try again, you have " + i + " guesses left."); 
+        alert("try again, you have " + i + " guesses left.");
+    }
 
 }
 
 if (i < 0) {
-    console.log("the things in my car: " + things);
+    console.log("the things in my car are: " + things);
+    alert("the things in my car are: " + things);
+
 }
 
 
