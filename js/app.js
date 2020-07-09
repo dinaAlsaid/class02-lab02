f5af670f206770c3d6cc38e23433c2195bef6a8b
dinaAlsaid <dinamalsaid@gmail.com>
2020-07-09 17:21:59 +0300

    final touches

--- js/app.js
+++ js/app.js
@@ -5,130 +5,54 @@ alert("this is a guessing game, you have to guess things about ME, dina");
 
 //// take the user's name
 var name = prompt("what is your name?");
-console.log("what is your name?");
-console.log("hello " + name + " !! let's play.");
 alert("hello " + name + "!! let's play.");
 
 
 ////-----question 1-----////
-function firstFifthQuestions(message) {
-    var answer = prompt(message);
-    if (answer === "yes") {
-        alert('you are wrong');
-    }
-    else {
+function firstFifthQuestions(message, correct) {
+    var answer = prompt(message).toLowerCase();
+    if (answer === correct) {
         alert('you are right');
         correctAnswers++;
+    }
+    else {
+        alert('you are wrong');
 
     }
 }
-firstFifthQuestions("Am I younger than 25?");
-firstFifthQuestions("Do I like cats?");
-firstFifthQuestions("Do I drink coffee?");
-firstFifthQuestions('Have I ever climbed a tree?');
-firstFifthQuestions('Am I shorter that 163 cm ?');
-// var age = prompt("am I older than 25 years old?");
-// console.log("am I older than 25 years old?");
+firstFifthQuestions("Am I younger than 25?","yes");
+firstFifthQuestions("Do I like cats?","yes");
+firstFifthQuestions("Do I drink coffee?","no");
+firstFifthQuestions('Have I ever climbed a tree?',"no");
+firstFifthQuestions('Am I shorter that 163 cm ?',"yes");
 
-// if (age.toLowerCase() == "no" || age.toLowerCase() == "n") {
-//     correctAnswers++;
-//     console.log("you're right, I am 24");
-//     alert("you're right, I am 24");
-// } else {
-//     console.log("Nope, I am 24");
-//     alert("Nope, I am 24");
-
-// }
-
-// ////-----question 2-----////
-// var cat = prompt("do I like cats?");
-// console.log("do I like cats?");
-
-// if (cat.toLowerCase() === "yes" || cat.toLowerCase() === "y") {
-//     correctAnswers++;
-//     console.log("correct");
-//     alert("correct");
-// } else {
-//     console.log("wrong, i do like cats");
-//     alert("wrong, I LOVE cats!!");
-
-// }
-
-// ////-----question 3-----////
-// var coffee = prompt("do I drink coffee");
-// console.log("do I drink coffee");
-
-// if (coffee.toLowerCase() === "no" || coffee.toLowerCase() === "n") {
-//     correctAnswers++;
-//     console.log("correct");
-//     alert("correct");
-// } else {
-//     console.log("wrong, Coffee is a no-no to me");
-//     alert("wrong, Coffee is a no-no to me");
-
-// }
-
-// ////------question 4 -----////
-// var climb = prompt("have I ever climbed a tree?");
-// console.log("have I ever climbed a tree?");
-
-// if (climb.toLowerCase() === "no" || climb.toLowerCase() === "n") {
-//     correctAnswers++;
-//     console.log("correct");
-//     alert("correct");
-// } else {
-//     console.log("wrong, i have weak arms");
-//     alert("wrong, I have weak arms");
-
-// }
-
-// ////-----question 5-----////
-// var height = prompt("Am I shorter than 163cm?");
-// console.log("Am I shorter than 163cm?");
-
-// if (height.toLowerCase() === "yes" || height.toLowerCase() === "y") {
-//     correctAnswers++;
-//     console.log("correct, i am 160 cm tall");
-//     alert("correct, i am 160 cm tall");
-// } else {
-//     console.log("wrong, i am 160 cm tall");
-//     alert("wrong, i am 160 cm tall");
-
-// }
 
 ////-----question 6-----////
 function sixthQuestion(){
 
 
 var howManyCats = prompt("How mant cats I had in my life? you have 4 guesses");
-console.log("How mant cats I had in my life? you have 4 guesses");
 
 for (var c = 1; c <= 3; c++) {
     if (howManyCats > 6) {
-        console.log("too high");
         howManyCats = prompt("too high,try again");
     } else if (howManyCats < 6) {
-        console.log("too low");
 
         howManyCats = prompt("too low, try again");
 
     } else if (howManyCats == 6) {
-        //compares a string with a number
+        correctAnswers++;
         break;
     }
 }
 if (howManyCats != 6) {
-    console.log("you're out of guesses, the correct answer is 6");
     alert("you're out of guesses, the correct answer is 6");
-} else {
-    correctAnswers++;
-
-    console.log("you're correct");
-    alert("you're correct");
+} 
 }
 
-}
+
 sixthQuestion();
+
 ////-----question 7-----////
 var flag = false;
 var things = ["napkin", "mask", "glove", "higeen", "key", "book", "cup"];
@@ -136,10 +60,9 @@ function seventhQuestion(){
 
 
 var guessThings;
-console.log("can you guess one thing i always have in my car these days?");
 
 for (var i = 5; i >= 0; i--) { //for number of guesses
-    guessThings = prompt("can you guess one thing i always have in my car these days?");
+    guessThings = prompt("can you guess one thing i always have in my car these days?").toLowerCase();
 
     for (var index = 0; index < things.length; index++) {
         //to go through the list
@@ -149,16 +72,13 @@ for (var i = 5; i >= 0; i--) { //for number of guesses
     }
     if (flag) {
         correctAnswers++;
-        console.log("correct");
         alert("correct");
         break;
     } else {
         if (i === 0) {
-            console.log("you have no guesses left. the things in my car are: " + things);
             alert("you have no guesses left. the things in my car are: " + things);
 
         } else {
-            console.log("try again, you have " + i + " guesses left.");
             alert("try again, you have " + i + " guesses left.");
         }
     }
